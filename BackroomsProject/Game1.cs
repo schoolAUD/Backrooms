2fc8afd60fcb7c7bfc5e0d8be824c6d72835d6d6
schoolAUD <logan.cookson1@icloud.com>
2024-10-16 14:15:53 +0100

    Update Game1.cs

--- BackroomsProject/Game1.cs
+++ BackroomsProject/Game1.cs
@@ -41,13 +41,6 @@ namespace BackroomsProject
             base.Initialize();
         }
 
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-        }
-
         protected override void Update(GameTime gameTime)
         {
             KeyboardState state = Keyboard.GetState();
